ebcbb20588723c05d417b132ff7dec45237e2f69
OohelloworldoO <sunny1234860@gmail.com>
2026-03-25 12:43:37 +0800

    udpate

--- DataStructure/Stack.c
+++ DataStructure/Stack.c
@@ -1,6 +1,22 @@
 #include<stdio.h>
 #include<stdbool.h>
 int main()
-{
+{ 
+    //Define the maximum capacity of the stack
+    #define MAX_SIZE 100 
+
+    //Define a structure for the stack
+    typedef struct
+    {   
+        int arr[MAX_SIZE];
+        int top;
+    }Stack;
+
+    //Function to initialization the stack
+    void initialize(Stack *stack)
+    {
+        stack -> top = -1;
+    }
+    
 
 }
