32f50e8d7a77e5f032418859e04451201f642af8
OohelloworldoO <sunny1234860@gmail.com>
2026-04-14 21:47:09 +0800

    update

---
+++ C_Fundamentals/array2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int i, n[5];
+    char input[20];
+
+    for (i = 1; i <= 5; i++)
+    {
+        printf("%d: ", i);
+        scanf("%d", &n[i - 1]);
+    }
+
+    for (i = 1; i <= 5; i++)
+    {
+        printf("%d\n", n[i - 1]);
+    }
+
+    while (1)
+    {
+        printf("Q: ");
+        scanf("%19s", input);
+
+        if (strcmp(input, "done") == 0)
+        {
+            break;
+        }
+
+        int index;
+        if (sscanf(input, "%d", &index) != 1)
+        {
+            printf("Invalid input\n");
+            continue;
+        }
+
+        if (index < 1 || index > 5)
+        {
+            printf("Out of range\n");
+            continue;
+        }
+
+        printf("%d\n", n[index - 1]);
+    }
+
+    return 0;
+}
