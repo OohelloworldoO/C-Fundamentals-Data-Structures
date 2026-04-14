50a85a56f6626fe53dd6000ef73aea07e1ffd724
OohelloworldoO <sunny1234860@gmail.com>
2026-04-14 22:23:56 +0800

    update

--- C_Fundamentals/array2.c
+++ C_Fundamentals/array2.c
@@ -15,7 +15,7 @@ int main(void)
     while (1)
     {
         printf("Q: ");
-        scanf("%19s", input);
+        scanf("%19s", input);//最多讀取19個字元
 
         if (strcmp(input, "done") == 0)
         {
@@ -40,3 +40,9 @@ int main(void)
 
     return 0;
 }
+/*
+strcmp比較兩個字串 如果相同則 strcmp("a", "b") == 0 else != 0
+strncmp(a, b, n);
+strcpy(dest, src);
+sscanf(s, "%d", &x);
+*/
