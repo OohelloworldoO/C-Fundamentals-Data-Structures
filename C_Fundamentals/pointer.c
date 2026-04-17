b52137aa7fcd16e393b5d8fce1e97e2747919a61
OohelloworldoO <sunny1234860@gmail.com>
2026-04-17 22:10:10 +0800

    update

--- C_Fundamentals/pointer.c
+++ C_Fundamentals/pointer.c
@@ -3,7 +3,7 @@ int main()
 {
     int i, j = 60;
     int *PointerJ = &j;
-    printf("%d\n", *PointerJ);  //印出*Pointer這個記憶體位址上的值
+    printf("%d\n", *PointerJ);  // 透過間接運算子 `*` ，取得指標 PointerJ 所指向物件的值
     printf("%d", PointerJ);
     return 0;
 }
