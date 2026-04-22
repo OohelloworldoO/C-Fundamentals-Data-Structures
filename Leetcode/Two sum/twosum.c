6477f26fd9cc3d57aa6aea135e71979f390596fd
OohelloworldoO <sunny1234860@gmail.com>
2026-04-22 21:44:13 +0800

    update

--- Leetcode/Two sum/twosum.c
+++ Leetcode/Two sum/twosum.c
@@ -1,6 +1,9 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     // 1. 配置記憶體空間
-    int* result = (int*)malloc(2 * sizeof(int));
+    int* result = (int*)malloc(2 * sizeof(int)); // sizeof(int) 為告訴電腦"一個整數"占用多少空間、(int *) 是強制轉型，用來告訴電腦這塊空間當作裝 int 陣列使用 因為 malloc 剛申請到空間時 只知道這是一塊空白空間，不知道要存甚麼
     
     // 2. 雙層迴圈尋找組合
     for (int i = 0; i < numsSize; i++) {
@@ -18,3 +21,6 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     }
     
     // 5. 若沒找到，設定 returnSize 為 0
+    *returnSize = 0;
+    return NULL;
+}
