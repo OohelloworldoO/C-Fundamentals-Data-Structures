8b72f95f756caee03d2d577d0908b6356db5b3c5
OohelloworldoO <sunny1234860@gmail.com>
2026-04-30 23:51:28 +0800

    update

---
+++ DataStructure/Linked List/linked_list_test.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} Node;
+
+
+Node *createNode(int value)
+{
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertAtHead(Node **head, int value)
+{
+    Node *newNode = createNode(value);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+void printfList(Node *head)
+{
+    Node *current = head;
+    while(current != NULL)
+    {
+        printf("[%d]->", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+int main(void)
+{
+    Node *head = NULL;
+    int size_of_linked_list;
+    printf("please input ur size of linked list: ");
+    scanf("%d", &size_of_linked_list);
+    printf("pleas input ur value:");
+    for(int i = 0; i < size_of_linked_list; i++)
+    {
+        int temp;
+        scanf("%d", &temp);
+        insertAtHead(&head, temp);
+    }
+    printfList(head);
+    return 0;
+}
